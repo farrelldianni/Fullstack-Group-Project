8c9e804c07a1e179039c5765568138fbf5c82e15
fghoneycutt <fghoneycutt@gmail.com>
2022-09-13 00:57:20 -0400

    small changes

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const routes = require("./controllers/");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const sequelize = require("./config/connection.js");
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.listen(PORT, () => console.log("Now listening"));
