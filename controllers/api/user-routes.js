d873cf19028f58622eec9c971a51244b099d7b60
fghoneycutt <fghoneycutt@gmail.com>
2022-09-14 01:37:14 -0400

    able to log in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-// const { User } = require("../../models/User");
 const User = require("../../models/User");
 
 // GET /api/users
@@ -55,7 +54,30 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
 
+    res.json({ user: dbUserData });
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    // res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
