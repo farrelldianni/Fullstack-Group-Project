58961f734d4581de53354a072a1d0b34006d06fc
fghoneycutt <fghoneycutt@gmail.com>
2022-09-14 22:39:04 -0400

    trying to fix comment routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -34,7 +34,6 @@ router.get("/:id", (req, res) => {
 
 // POST /api/users
 router.post("/", (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
