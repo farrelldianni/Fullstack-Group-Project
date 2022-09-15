4db365d8069173ccb7a08173faccd03c25b6f562
rodriguez531 <alexr5408@gmail.com>
2022-09-15 01:05:06 -0400

    got handlebars to work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 
+
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("login");
 });
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const path = require("path");
-const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
-const routes = require("./controllers/");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require("./config/connection.js");
-const routes = require("./routes/");
+const routes = require("./controllers/");
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) =>
+{
+res.render("login")
+})
 
+app.get("/home", (req, res) => 
+{
+  res.render("homepage")
+})
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
